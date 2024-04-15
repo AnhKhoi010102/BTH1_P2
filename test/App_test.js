7c0aa59597527ac92972f085d5d47cbf81a24321
Your Name <you@example.com>
2024-04-15 08:09:44 +0700

    first commit

---
+++ test/App_test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native'
+import App from "../App";
+const App = ()=>
+{
+    return (
+        <App/>
+    );
+}
+
+export default App;
